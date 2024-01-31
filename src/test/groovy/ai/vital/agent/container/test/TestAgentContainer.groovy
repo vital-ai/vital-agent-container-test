02ba3cf25a25d9abf50b477a91411911a6cbfb85
Marc Hadfield <marc@vital.ai>
2024-01-31 13:31:25 -0500

    sync

---
+++ src/test/groovy/ai/vital/agent/container/test/TestAgentContainer.groovy
@@ -0,0 +1,18 @@
+package ai.vital.agent.container.test
+
+class TestAgentContainer {
+
+    static void main(String[] args) {
+
+        TestAgentContainer test = new TestAgentContainer()
+
+        test.runTest(args)
+    }
+
+    public void runTest(args) {
+
+        println "Hello World"
+        
+    }
+
+}
